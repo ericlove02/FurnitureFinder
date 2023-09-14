80d2120116f26aa555a5500a5d823b0c05f32523
ericlove02 <eric.love02@yahoo.com>
2023-09-13 19:28:13 -0500

    attempted ar demo, only works on localhost

--- FurnitureFinder/pages/ARSearch.js
+++ FurnitureFinder/pages/ARSearch.js
@@ -1,15 +1,48 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, Text } from 'react-native';
+import WebView from 'react-native-webview';
+import * as Permissions from 'expo-permissions';
 
 const ARSearch = ({ navigation, route }) => {
-    const vibe = route.params.vibe;
+  const vibe = route.params.vibe;
 
-    return (
-        <View>
-            <Text>AR Search</Text>
-            <Text>Your Vibe: {vibe}</Text>
-        </View>
-    );
+  // Request camera permission
+  useEffect(() => {
+    (async () => {
+      const { status } = await Permissions.askAsync(Permissions.CAMERA);
+      if (status !== 'granted') {
+        console.error('Camera permission not granted');
+      }
+    })();
+  }, []);
+
+  // AR.js HTML content to render in the WebView
+  const arJsHtml = `
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <script src="https://aframe.io/releases/1.2.0/aframe.min.js"></script>
+        <script src="https://cdn.rawgit.com/jeromeetienne/AR.js/2.0.8/aframe/build/aframe-ar.js"></script>
+      </head>
+      <body style="margin: 0; overflow: hidden;">
+        <a-scene embedded arjs="sourceType: webcam; debugUIEnabled: false;">
+          <a-box position="0 0 -1" color="tomato"></a-box>
+        </a-scene>
+      </body>
+    </html>
+  `;
+
+  return (
+    <View style={{ flex: 1 }}>
+      <Text>AR Search</Text>
+      <Text>Your Vibe: {vibe}</Text>
+      <WebView
+        style={{ flex: 1 }}
+        originWhitelist={['*']}
+        source={{ html: arJsHtml }}
+      />
+    </View>
+  );
 };
 
 export default ARSearch;

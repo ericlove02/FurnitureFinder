e2186b4230bb5c5fe2eb2c3836d51d815bd71f14
alex9947 <tung_896404@tamu.edu>
2023-09-20 22:46:03 -0500

    added variables to api call

--- FurnitureFinder/gptScript.py
+++ FurnitureFinder/gptScript.py
@@ -3,16 +3,22 @@ import openai
 # Your API key
 openai.api_key = "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"
 
+product_description = input("Enter a product description: ") # TODO - pull from Wayfair API
+vibe = input("Enter a vibe: ") # TODO - add a list of vibes to choose from
+
 response = openai.ChatCompletion.create(
   model="gpt-3.5-turbo",
   messages=[
     {
       "role": "system",
-      "content": "You will be provided with statements, and your task is to convert them to standard English."
+      "content": """Given a furniture/decoration product description and a vibe/theme word, 
+                    please rate it on a scale of 1-10 with 1 being the total opposite of the vibe/theme and
+                    10 being a spot on match of the vibe/theme. Return just the number rating."""
     },
     {
       "role": "user",
-      "content": "She no went to the market."
+      "content": f"""Product description: {product_description} 
+                     Vibe: {vibe}"""
     }
   ],
   temperature=0,
@@ -21,3 +27,7 @@ response = openai.ChatCompletion.create(
   frequency_penalty=0,
   presence_penalty=0
 )
+
+response_text = response['choices'][0]['message']['content']
+
+print(response_text)

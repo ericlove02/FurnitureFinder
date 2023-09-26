d081e1dce6f1a6488d057bf1288f7d962f444cda
ericlove02 <eric.love02@yahoo.com>
2023-09-26 11:07:26 -0500

    ar setup

--- FurnitureFinder/pages/ARSearch.js
+++ FurnitureFinder/pages/ARSearch.js
@@ -1,15 +1,56 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+'use strict';
 
-const ARSearch = ({ navigation, route }) => {
-  const vibe = route.params.vibe;
+import React, { Component } from 'react';
+import { StyleSheet } from 'react-native';
+import {
+  ViroARScene,
+  ViroText,
+  ViroMaterials,
+  ViroBox,
+  Viro3DObject,
+  ViroAmbientLight,
+  ViroSpotLight,
+  ViroARPlaneSelector,
+  ViroNode,
+  ViroAnimations,
+} from 'react-viro';
+var createReactClass = require('create-react-class');
+var HelloWorldSceneAR = createReactClass({
+  getInitialState() {
+    return {
+      text: "Initializing AR..."
+    };
+  },
 
-  return (
-    <View>
-      <Text>AR Search</Text>
-      <Text>Your Vibe: {vibe}</Text>
-    </View>
-  );
-};
+  render: function () {
+    return (
+      <ViroARScene onTrackingInitialized={() => { this.setState({ text: "Hello World!" }) }}>
+        <ViroText text={this.state.text} scale={[.1, .1, .1]} height={1} width={4} position={[0, .5, -1]} style={styles.helloWorldTextStyle} />
 
-export default ARSearch;
+        <ViroAmbientLight color={"#aaaaaa"} />
+        <ViroSpotLight innerAngle={5} outerAngle={90} direction={[0, -1, -.2]} position={[0, 3, 1]} color="#ffffff" castsShadow={true} />
+
+        <Viro3DObject
+          source={require('./res/emoji_smile/emoji_smile.vrx')}
+          position={[0, 0, -1]}
+          scale={[.2, .2, .2]}
+          type="VRX"
+          dragType="FixedDistance" onDrag={() => { }}
+        />
+
+      </ViroARScene>
+    );
+  },
+});
+
+var styles = StyleSheet.create({
+  helloWorldTextStyle: {
+    fontFamily: 'Arial',
+    fontSize: 50,
+    color: '#ffffff',
+    textAlignVertical: 'center',
+    textAlign: 'center',
+  },
+});
+
+module.exports = HelloWorldSceneAR;

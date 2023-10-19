25de9fb9fed90453ddb85f1b29e10b52cca4874a
Eric Love <eric.love02@yahoo.com>
2023-10-19 03:31:54 -0500

    start of landing and home ui

--- README.md
+++ README.md
@@ -1,16 +1,5 @@
 # FurnitureFinder
 
-To run the project, navigate to the directory and run one of the following npm commands.
-```
-- cd FurnitureFinder
-- npm run android
-- npm run ios  # you need to use macOS to build the iOS project - use the Expo app if you need to do iOS development without a Mac
-- npm run web
-```
-or to run in the Expo Go App on iOS or Andriod do 
-```
-npx expo start
-```
-and you can connect to the project on your mobile device.
-
-To set up an android emulator follow (these)[https://docs.expo.dev/workflow/android-studio-emulator/] instructions or for an iOS emulator use XCode on mac.
+To work on the project, download Unity 2022.3.11f1.
+Clone the repo and in Unity Hub select Open > Add Project from Disk.
+Select the chAiR directory.
---
+++ chAiR/Assets/Scripts/VibeHandler.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class SetVibe : MonoBehaviour
+{
+    public TMP_Dropdown vibeDropdown;
+
+    public void Start()
+    {
+        if (SceneManager.GetActiveScene().name == "LandingScene")
+        {
+            PlayerPrefs.DeleteAll();
+        }
+
+        vibeDropdown.onValueChanged.AddListener(OnDropdownValueChanged);
+
+        int selectedVibeIndex = PlayerPrefs.GetInt("SelectedVibeIndex", 0);
+        vibeDropdown.value = selectedVibeIndex;
+    }
+
+    void OnDropdownValueChanged(int index)
+    {
+        PlayerPrefs.SetInt("SelectedVibeIndex", index);
+        PlayerPrefs.SetString("Vibe", vibeDropdown.options[index].text);
+        PlayerPrefs.Save();
+    }
+}

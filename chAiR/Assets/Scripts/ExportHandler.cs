a02a849876d7f1fe9165578975685cdd3ad73b15
Eric Love <eric.love02@yahoo.com>
2023-10-30 18:57:01 -0500

    export image display pop up

--- chAiR/Assets/Scripts/ExportHandler.cs
+++ chAiR/Assets/Scripts/ExportHandler.cs
@@ -1,10 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using NativeGalleryNamespace;
+using TMPro;
 
 public class ExportHandler : MonoBehaviour
 {
+    [SerializeField] private GameObject imageDisplayPanel;
+    [SerializeField] private Image imageDisplay;
+    [SerializeField] private Button closeButton;
+
     public void CaptureImage()
     {
         ScreenCapture.CaptureScreenshot("export.png");
@@ -21,10 +27,25 @@ public class ExportHandler : MonoBehaviour
         if (System.IO.File.Exists(screenshotPath))
         {
             NativeGallery.SaveImageToGallery(screenshotPath, "MyGallery", "chAiR.png");
+
+            imageDisplayPanel.SetActive(true);
+            imageDisplayPanel.transform.localPosition = Vector3.zero;
+
+            Texture2D texture = NativeGallery.LoadImageAtPath(screenshotPath, 512);
+            if (texture != null)
+            {
+                imageDisplay.sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), Vector2.one * 0.5f);
+            }
         }
         else
         {
             Debug.LogWarning("Screenshot file not found.");
         }
+    }
+
+    public void ClosePanel()
+    {
+        imageDisplayPanel.SetActive(false);
+        imageDisplayPanel.transform.localPosition = new Vector3(9999f, 0f, 0f);
     }
 }

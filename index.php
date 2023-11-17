ee3a6b1eec5b2abf7eb8c97f4c5551cc095d0469
alex9947 <tung_896404@tamu.edu>
2023-11-17 17:45:24 -0600

    updated php code

--- index.php
+++ index.php
@@ -22,22 +22,45 @@ function CloseCon($conn)
         $stmt->bind_param("s", $_GET['vibe_name']);
         $stmt->execute();
         $result = $stmt->get_result();
-        $row = mysqli_fetch_array($result);
+        $vibe_row = $result->fetch_assoc();
+
+        if ($vibe_row) {
+            // Get the associated FUR_ID values from the furniturevibes table
+            $stmt2 = $conn->prepare("SELECT FUR_ID FROM furniturevibes WHERE VIBE_ID = ?");
+            $stmt2->bind_param("i", $vibe_row['VIBE_ID']);
+            $stmt2->execute();
+            $result2 = $stmt2->get_result();
         
-        $stmt2 = $conn->prepare("SELECT FUR_ID FROM furniturevibes WHERE VIBE_ID = ?");
-        $stmt2->bind_param("s", $row[0]);
-        $stmt2->execute();
-        $result2 = $stmt2->get_result();
-        $row2 = mysqli_fetch_array($result2);
+            // Use a prepared statement with IN clause to retrieve all associated furniture
+            $placeholders = implode(',', array_fill(0, $result2->num_rows, '?'));
+            $stmt3 = $conn->prepare("SELECT * FROM furniture WHERE FUR_ID IN ($placeholders)");
+            
+            // Bind parameters dynamically
+            $params = array();
+            $types = "";
+            while ($row2 = $result2->fetch_assoc()) {
+                $params[] = &$row2['FUR_ID'];
+                $types .= "i";
+            }
+            array_unshift($params, $types);
+            call_user_func_array(array($stmt3, 'bind_param'), $params);
         
-        $stmt3 = $conn->prepare("SELECT * FROM furniture WHERE FUR_ID = ?");
-        $stmt3->bind_param("s", $row2[0]);
-        $stmt3->execute();
-        $result3 = $stmt3->get_result();
-        $outp = $result3->fetch_all(MYSQLI_ASSOC);
+            $stmt3->execute();
+            $result3 = $stmt3->get_result();
+            $outp = $result3->fetch_all(MYSQLI_ASSOC);
         
-        $myJSON = json_encode($outp);
-        echo $myJSON;
+            // Output your data as needed
+            echo json_encode($outp);
+        } else {
+            // Handle the case where the given vibe_name doesn't exist
+            echo "Vibe not found";
+        }
+        
+        // Close your statements and connection as needed
+        $stmt->close();
+        $stmt2->close();
+        $stmt3->close();
+        
         CloseCon($conn);
     }
 ?>

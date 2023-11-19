15af82b6d22badaaef9c7239e45d155df0094e55
pres1017 <pres1017@gmail.com>
2023-11-18 19:29:51 -0600

    AllFurn reads given prefabs and provides results

--- chAiR/Assets/Scripts/ScrollHandler.cs
+++ chAiR/Assets/Scripts/ScrollHandler.cs
@@ -3,9 +3,10 @@ using UnityEngine.UI;
 
 public class ScrollViewManager : MonoBehaviour
 {
-    public GameObject[] prefab; // Prefab of the object you want to add to the scroll view
+    public GameObject prefab; // Prefab of the object you want to add to the scroll view
     public Transform content; // Content transform of the scroll view
-    public int numberOfObjects = 10; // Number of objects you want to add
+    // public int numberOfObjects = 10; // Number of objects you want to add
+    public GameObject[] furnitureItems;
 
     void Start()
     {
@@ -16,18 +17,19 @@ public class ScrollViewManager : MonoBehaviour
     void PopulateScrollView()
     {
         // Loop through the number of objects you want to add
-        for (int i = 0; i < numberOfObjects; i++)
+        foreach(GameObject furniture in furnitureItems)
         {
-            // Instantiate the prefab
             GameObject newObject = Instantiate(prefab, content);
-
-            // You can customize the newObject properties here if needed
-            // For example, set the text of a Text component if your prefab has one
+            Text textComponent = newObject.GetComponentInChildren<Text>();
+            if (textComponent != null)
+            {
+                textComponent.text = furniture.name;
+            }
         }
 
         // Calculate the size of the content based on the number of objects and their size
         RectTransform contentRect = content.GetComponent<RectTransform>();
-        float contentHeight = numberOfObjects * prefab.GetComponent<RectTransform>().rect.height;
+        float contentHeight = furnitureItems.Length * prefab.GetComponent<RectTransform>().rect.height;
         contentRect.sizeDelta = new Vector2(contentRect.sizeDelta.x, contentHeight);
     }
 }

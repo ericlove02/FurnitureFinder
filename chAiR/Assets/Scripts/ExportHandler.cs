3a0424c827e5964f572a8384e84165f67c3e90b0
brycewatt <brycewatt@gmail.com>
2023-11-19 18:07:49 -0600

    added an export options ui before saving image

--- chAiR/Assets/Scripts/ExportHandler.cs
+++ chAiR/Assets/Scripts/ExportHandler.cs
@@ -10,15 +10,19 @@ public class ExportHandler : MonoBehaviour
     [SerializeField] private GameObject imageDisplayPanel;
     [SerializeField] private Image imageDisplay;
     [SerializeField] private Button closeButton;
+    [SerializeField] private GameObject imageExportPanel;
+    [SerializeField] private Image imageExport;
+    [SerializeField] private Button saveButton;
+    [SerializeField] private Button cancelButton;
+    [SerializeField] private Button shareButton;
 
     public void CaptureImage()
     {
         ScreenCapture.CaptureScreenshot("export.png");
-        StartCoroutine(SaveScreenshot());
+        StartCoroutine(DisplayScreenshot());
     }
 
-    // Coroutine to save screenshot to device after a delay
-    private IEnumerator SaveScreenshot()
+    private IEnumerator DisplayScreenshot()
     {
         yield return new WaitForSeconds(1f);
 
@@ -26,7 +30,32 @@ public class ExportHandler : MonoBehaviour
 
         if (System.IO.File.Exists(screenshotPath))
         {
+            imageExportPanel.SetActive(true);
+            imageExportPanel.transform.localPosition = Vector3.zero;
+
+            Texture2D texture = NativeGallery.LoadImageAtPath(screenshotPath, 512);
+            if (texture != null)
+            {
+                imageExport.sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), Vector2.one * 0.5f);
+            }
+        }
+        else
+        {
+            Debug.LogWarning("Screenshot file not found.");
+        }
+    }
+
+    // Coroutine to save screenshot to device after a delay
+    public void SaveScreenshot()
+    {
+        string screenshotPath = Application.persistentDataPath + "/export.png";
+
+        if (System.IO.File.Exists(screenshotPath))
+        {
             NativeGallery.SaveImageToGallery(screenshotPath, "MyGallery", "chAiR.png");
+
+            imageExportPanel.SetActive(false);
+            imageExportPanel.transform.localPosition = new Vector3(9999f, 0f, 0f);
 
             imageDisplayPanel.SetActive(true);
             imageDisplayPanel.transform.localPosition = Vector3.zero;
@@ -47,5 +76,11 @@ public class ExportHandler : MonoBehaviour
     {
         imageDisplayPanel.SetActive(false);
         imageDisplayPanel.transform.localPosition = new Vector3(9999f, 0f, 0f);
+    }
+
+    public void CancelExport()
+    {
+        imageExportPanel.SetActive(false);
+        imageExportPanel.transform.localPosition = new Vector3(9999f, 0f, 0f);
     }
 }

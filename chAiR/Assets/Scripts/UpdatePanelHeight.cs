2cf0890986fce771c004ce2a9e9ea5a11f388eab
Eric Love <eric.love02@yahoo.com>
2023-11-28 01:19:17 -0600

    updated script name to fix clash w global defs

--- chAiR/Assets/Scripts/UpdatePanelHeight.cs
+++ chAiR/Assets/Scripts/UpdatePanelHeight.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FixHeight : MonoBehaviour
+public class UpdatePanelHeight : MonoBehaviour
 {
     void Update()
     {
